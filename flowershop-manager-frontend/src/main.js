fe8994733b542e9030f8c512370a965fd45bab82
Paul <paul.gui1306@gmail.com>
2025-05-21 12:46:49 +0300

    Initial commit

---
+++ flowershop-manager-frontend/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ flowershop-manager-frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        background: '#1e1e2f',
+        cards: '#33334d',
+        text_primary: '#f4f4f4',
+        text_secondary: '#b0b0c3',
+        text_accents: '#2e2e2e',
+        divider: '#44445d',
+        accent1: '#d16ba5',
+        accent2: '#88d498',
+        accent3: '#9d79bc',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
