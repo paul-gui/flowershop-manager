965dce6fd94fd9fee7853184086851bed9f59def
paul-gui <paul.gui1306@gmail.com>
2025-05-25 23:41:08 +0300

    Warehouses BE & FE + authentication % role protection
    Warehouses implemented partially

---
+++ flowershop-manager-frontend/src/services/WarehousesService.ts
@@ -0,0 +1,9 @@
+import api from "@/services/API";
+import { WarehouseDto } from "@/dtos/dataInputDTOs";
+
+const baseUrl = 'Warehouses'
+
+export async function addWarehouse(warehouse: WarehouseDto){
+    const res = await api.post(baseUrl + '/CreateWarehouse', warehouse)
+    return res.data
+}
