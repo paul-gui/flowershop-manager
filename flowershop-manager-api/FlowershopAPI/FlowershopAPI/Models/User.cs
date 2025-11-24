39e4346e2aa23ec79778f9021ddb6acbbd6b014a
paul-gui <paul.gui1306@gmail.com>
2025-11-24 13:00:52 +0200

    Added modifiers in contracts in backend

--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Models/User.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Models/User.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace FlowershopAPI.Models
 {
     public class User : IdentityUser
     {
-        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
         public Role Role { get; set; }
         public DateTime CreatedAt { get; set; }
     }
--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Models/Warehouse.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Models/Warehouse.cs
@@ -1,9 +1,15 @@
-﻿namespace FlowershopAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowershopAPI.Models
 {
     public class Warehouse
     {
         public Guid Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public required string Name { get; set; }
-        public ICollection<Product> Products { get; set; }
+        
+        public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
