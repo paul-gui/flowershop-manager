29bfd6f767913ede24ce9f2fbce99097de0e0b86
paul-gui <paul.gui1306@gmail.com>
2025-11-25 11:09:37 +0200

    Added return types to service methods in frontend

--- flowershop-manager-frontend/src/services/WarehousesService.ts
+++ flowershop-manager-frontend/src/services/WarehousesService.ts
@@ -3,30 +3,31 @@ import type {
   CreateWarehouseRequest,
   UpdateWarehouseRequest,
 } from '@/types/dtos/warehouse/warehouseRequests.dto.ts'
+import type { WarehouseDetailsResponse, WarehouseResponse } from '@/types/dtos/warehouse/warehouseResponses.dto.ts'
 
 const baseUrl = 'Warehouses'
 
-export async function createWarehouse(createWarehouseRequest: CreateWarehouseRequest){
+export async function createWarehouse(createWarehouseRequest: CreateWarehouseRequest):Promise<WarehouseDetailsResponse>{
     const res = await api.post(baseUrl + '/CreateWarehouse', createWarehouseRequest)
     return res.data
 }
 
-export async function getWarehouses(){
+export async function getWarehouses():Promise<WarehouseResponse[]>{
     const res = await api.get(baseUrl + '/GetWarehouses');
     return res.data
 }
 
-export async function getWarehouse(id: string){
+export async function getWarehouse(id: string):Promise<WarehouseDetailsResponse>{
     const res = await api.get(baseUrl + '/GetWarehouse/' + id);
     return res.data
 }
 
-export async function updateWarehouse(updateWarehouseRequest: UpdateWarehouseRequest){
+export async function updateWarehouse(updateWarehouseRequest: UpdateWarehouseRequest):Promise<WarehouseDetailsResponse>{
     const res = await api.put(baseUrl + '/UpdateWarehouse/', updateWarehouseRequest)
     return res.data
 }
 
-export async function deleteWarehouse(id: string){
+export async function deleteWarehouse(id: string):Promise<WarehouseDetailsResponse>{
     const res = await api.delete(baseUrl + '/DeleteWarehouse/' + id);
     return res.data
 }
