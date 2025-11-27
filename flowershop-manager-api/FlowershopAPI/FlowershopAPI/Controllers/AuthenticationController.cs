891cd97973a47649b1048a7e7dd1e43ca00020c9
paul-gui <97873736+paul-gui@users.noreply.github.com>
2025-11-27 14:04:52 +0200

    chore: create manager for authentication (#25)

--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Controllers/AuthenticationController.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Controllers/AuthenticationController.cs
@@ -7,71 +7,42 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using FlowershopAPI.Contracts.Authentication;
+using FlowershopAPI.Managers.Authentication.Contract;
 
 namespace FlowershopAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticationController(UserManager<User> userManager, IConfiguration configuration)
+    public class AuthenticationController(IAuthenticationManager authenticationManager)
         : ControllerBase
     {
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterAccountRequest request)
         {
-            var user = new User
-            {
-                UserName = request.Email,
-                Email = request.Email,
-                Name = request.Name
-            };
+            var result = await authenticationManager.Register(request);
 
-            var result = await userManager.CreateAsync(user, request.Password);
-
-            if (!result.Succeeded)
+            if (result == null)
             {
-                return BadRequest(result.Errors);
+                return BadRequest();
             }
 
-            await userManager.AddToRoleAsync(user, "User");
-
-            return Ok("User registered");
+            return Ok(result);
         }
 
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
         {
-            var user = await userManager.FindByNameAsync(loginRequest.Email);
-            if (user == null || !await userManager.CheckPasswordAsync(user, loginRequest.Password))
+            var result = await authenticationManager.Login(loginRequest);
+
+            if (result == null)
             {
                 return Unauthorized(new
                 {
                     message = "Invalid email or password"
                 });
             }
-
-            var roles = await userManager.GetRolesAsync(user);
-            var roleClaims = roles.Select(r => new Claim(ClaimTypes.Role, r));
-
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, user.UserName),
-                new Claim(ClaimTypes.NameIdentifier, user.Id)
-            }.Union(roleClaims);
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                issuer: configuration["Jwt:Issuer"],
-                audience: configuration["Jwt:Audience"],
-                claims: claims,
-                expires: DateTime.UtcNow.AddHours(1),
-                signingCredentials: credentials);
-
-            return Ok(new
-            {
-                token = new JwtSecurityTokenHandler().WriteToken(token)
-            });
+            
+            return Ok(result);
         }
     }
 }
