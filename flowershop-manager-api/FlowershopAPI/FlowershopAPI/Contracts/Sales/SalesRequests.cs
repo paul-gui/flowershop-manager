ba4af98c4ecdda924f83e1b538d5562ab26a10ab
paul-gui <paul.gui1306@gmail.com>
2025-11-30 18:33:48 +0200

    Created the basic DTOs for Sales

--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Sales/SalesRequests.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Sales/SalesRequests.cs
@@ -1,7 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FlowershopAPI.Contracts.Sales;
 
 public class SaleCreationRequest
 {
+    [Required]
     public required string UserId { get; set; }
     
+    [Required]
+    public required Guid ProductId { get; set; }
+    
+    [Required]
+    public required Guid DestinationId { get; set; }
+    
+    [Required]
+    public required int Quantity { get; set; }
+    
+    [Required]
+    public decimal PriceAtSale { get; set; }
 }
