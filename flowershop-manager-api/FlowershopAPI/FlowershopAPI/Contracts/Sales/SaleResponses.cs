3a6a1ffea48db60932c9a0b02ae8be6f52d941de
paul-gui <97873736+paul-gui@users.noreply.github.com>
2026-01-02 19:54:20 +0200

    feat:implement backend to get sales from database (#52)

--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Sales/SaleResponses.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Sales/SaleResponses.cs
@@ -2,5 +2,10 @@ namespace FlowershopAPI.Contracts.Sales;
 
 public class SaleResponse
 {
-    
+    public Guid Id { get; set; }
+    public string ProductName { get; set; }
+    public string WarehouseName { get; set; }
+    public string DestinationName { get; set; }
+    public int Quantity { get; set; }
+    public decimal PriceAtSale { get; set; }
 }
