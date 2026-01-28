376b6596b8cebfd6a955851dd948e12225c1bc4e
paul-gui <97873736+paul-gui@users.noreply.github.com>
2026-01-28 23:19:32 +0200

    feat: define authentication rules for pages (#22) - MERGE

---
+++ flowershop-manager-frontend/src/components/Common/UnauthorizedPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-full  p-4 py-12">
+    <div class="p-4 w-full bg-white rounded-lg">
+      <p>Nu aveti acces la aceasta pagina!</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
