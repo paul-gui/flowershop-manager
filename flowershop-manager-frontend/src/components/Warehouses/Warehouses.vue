5d54bf2a122620dd66627d3d76152076095c6c37
paul-gui <97873736+paul-gui@users.noreply.github.com>
2026-01-29 04:06:54 +0200

    feat: group admin functions into a button (#76) - MERGE

--- flowershop-manager-frontend/src/components/Warehouses/Warehouses.vue
+++ flowershop-manager-frontend/src/components/Warehouses/Warehouses.vue
@@ -3,27 +3,42 @@
     <div class="max-w-md mx-auto space-y-4 flex flex-col">
       <div class="flex flex-row justify-between items-baseline">
         <h1 class="text-h1 text-text_primary">Locatii</h1>
-        <button
-          class="h-12 aspect-square rounded-lg bg-cards hover:bg-divider"
-          @click="goToHistory"
-        >
-          <i class="fa fa-clock text-white text-2xl"></i>
-        </button>
+        <div class="space-x-2">
+          <button
+            class="h-12 aspect-square rounded-lg bg-cards hover:bg-divider"
+            @click="editingMode = !editingMode"
+          >
+            <i
+              :class="['fa text-white text-2xl',
+              editingMode
+                ? 'fa-x'
+                : 'fa-pen'
+              ]"></i>
+          </button>
+          <button
+            class="h-12 aspect-square rounded-lg bg-cards hover:bg-divider"
+            @click="goToHistory"
+          >
+            <i class="fa fa-clock text-white text-2xl"></i>
+          </button>
+        </div>
       </div>
-      <div class="space-y-2 max-h-[60%] overflow-y-scroll">
+      <div class="space-y-2 max-h-72 overflow-y-scroll">
         <div v-for="(warehouse, index) in warehouses" :key="index">
           <content-button
             :title="warehouse.name"
             icon="fa fa-pen"
             @primary-click="() => goToCreateSale(warehouse.id)"
             @secondary-click="() => goToEditWarehouse(warehouse.id)"
+            :showSecondButton="editingMode"
           ></content-button>
         </div>
       </div>
       <div class="flex items-center justify-center">
         <button
           class="border-gray-300 hover:bg-[#1a1a28] border border-dashed text-gray-300 py-3 px-8 rounded-lg"
-          v-on:click="goToAddWarehouse"
+          @click="goToAddWarehouse"
+          v-if="editingMode"
         >
           <i class="fa fa-plus"></i>
           Adauga
@@ -48,6 +63,7 @@ const props = defineProps({
   }
 })
 
+const editingMode = ref<boolean>(false);
 const warehouses = ref<WarehouseResponse[]>([])
 
 onMounted(async () => {
