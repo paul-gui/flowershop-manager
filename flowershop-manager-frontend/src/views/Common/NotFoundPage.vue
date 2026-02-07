29e7634126bfab1d068f65a42dcb1afa6777e63d
paul-gui <paul.gui1306@gmail.com>
2026-02-07 10:49:54 +0200

    Modified common pages 404 and 401

--- flowershop-manager-frontend/src/views/Common/NotFoundPage.vue
+++ flowershop-manager-frontend/src/views/Common/NotFoundPage.vue
@@ -4,6 +4,6 @@
 <template>
   <div class="h-full p-4 flex flex-col items-center justify-center">
     <span class="text-9xl text-[#3c3c5d]">404</span>
-    <span class="text-gray-300">Pagina nu a fost gasita!</span>
+    <span class="text-gray-300">Pagina nu a fost găsită!</span>
   </div>
 </template>
