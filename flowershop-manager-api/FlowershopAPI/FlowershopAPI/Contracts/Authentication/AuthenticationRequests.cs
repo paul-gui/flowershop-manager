a1e976893579e9d631532a94b252849f026d0779
Paul Gui <97873736+paul-gui@users.noreply.github.com>
2026-05-30 12:59:03 +0300

    chore: add data annotations for passwords in backend (#139)

--- flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Authentication/AuthenticationRequests.cs
+++ flowershop-manager-api/FlowershopAPI/FlowershopAPI/Contracts/Authentication/AuthenticationRequests.cs
@@ -9,9 +9,12 @@ public class RegisterAccountRequest
     public required string Email { get; set; }
     
     [Required]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.")]
     public required string Password { get; set; }
     
     [Required]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
     public required string PasswordConfirmation { get; set; }
     
     [Required]
@@ -32,21 +35,24 @@ public class ForgotPasswordRequest
 {
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 }
 
 public class ResetPasswordRequest
 {
     [Required]
-    public string Token { get; set; }
+    public required string Token { get; set; }
     
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public required string Email { get; set; }
     
     [Required]
-    public string Password { get; set; }
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.")]
+    public required string Password { get; set; }
     
     [Required]
-    public string PasswordConfirmation { get; set; }
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    public required string PasswordConfirmation { get; set; }
 }
